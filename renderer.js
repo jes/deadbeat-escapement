9e46e5ef7529a25e461fa13ec4701e6e5bf1f6a3
James Stanley <james@incoherency.co.uk>
2024-04-29 15:11:23 +0100

    Fix rotation centres, simplify context transformations

--- renderer.js
+++ renderer.js
@@ -3,12 +3,7 @@ class Renderer {
     world = null;
     canvasId = null;
 
-    // viewport
     viewHeight = 2.0; // metres
-    pxPerM = null;
-    mPerPx = null;
-    xCentre = null;
-    yCentre = null;
 
     ctx = null;
     canvas = null;
@@ -38,18 +33,17 @@ class Renderer {
             this.ctx = canvas.getContext('2d');
             this.ctx.save();
 
-            this.pxPerM = canvas.height / this.viewHeight;
-            this.mPerPx = 1.0 / this.pxPerM;
-            this.xCentre = this.canvas.width / 2;
-            this.yCentre = this.canvas.height / 2;
-
             this.world.step(dt);
 
             this.ctx.fillStyle = '#45f';
             this.ctx.strokeStyle = '#fff';
             this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
-            this.ctx.translate(this.xCentre, this.yCentre);
+            this.ctx.translate(this.canvas.width/2, this.canvas.height/2);
+
+            let scale = canvas.height / this.viewHeight;
+            this.ctx.lineWidth = 1/scale;
+            this.ctx.scale(scale, -scale);
      
             for (let body = world.getBodyList(); body; body = body.getNext()) {
                 this.renderBody(body);
@@ -68,30 +62,36 @@ class Renderer {
  
     renderBody(body) {
         for (let fixture = body.getFixtureList(); fixture; fixture = fixture.getNext()) {
+            this.ctx.save();
+            let bx = body.getPosition().x;
+            let by = body.getPosition().y;
+
+            // rotate around object centre
+            this.ctx.translate(bx, by);
+            this.ctx.rotate(body.getAngle());
+            this.ctx.translate(-bx, -by);
+
+            // translate to object position
+            this.ctx.translate(bx, by);
+
             this.renderFixture(body, fixture);
+            this.ctx.restore();
         }
     }
  
     renderFixture(body, fixture) {
-        let bx = body.getPosition().x * this.pxPerM;
-        let by = -body.getPosition().y * this.pxPerM;
-
-        this.ctx.save();
-        this.ctx.translate(-bx, by);
-        this.ctx.rotate(body.getAngle());
-        this.ctx.translate(bx, -by);
         this.ctx.beginPath();
 
         let shape = fixture.getShape();
         let type = fixture.getType();
         if (type == 'circle') {
-            this.ctx.arc(bx + shape.m_p.x * this.pxPerM, by - shape.m_p.y * this.pxPerM, shape.m_radius * this.pxPerM, 0, Math.PI*2, true);
+            this.ctx.arc(shape.m_p.x, shape.m_p.y, shape.m_radius, 0, Math.PI*2, true);
         } else if (type == 'polygon') {
             let p = shape.m_vertices;
             if (p.length > 1) {
-                this.ctx.moveTo(bx + p[0].x * this.pxPerM, by - p[0].y * this.pxPerM);
+                this.ctx.moveTo(p[0].x, p[0].y);
                 for (let i = 1; i < p.length; i++) {
-                    this.ctx.lineTo(bx + p[i].x * this.pxPerM, by - p[i].y * this.pxPerM);
+                    this.ctx.lineTo(p[i].x, p[i].y);
                 }
                 this.ctx.closePath();
             }
@@ -99,7 +99,6 @@ class Renderer {
 
         this.ctx.fill();
         this.ctx.stroke();
-        this.ctx.restore();
     }
  
     renderJoint(joint) {
@@ -107,10 +106,10 @@ class Renderer {
         if (type == 'distance-joint') {
             let posA = joint.m_bodyA.getPosition().clone().add(joint.m_localAnchorA);
             let posB = joint.m_bodyB.getPosition().clone().add(joint.m_localAnchorB);
-            let x1 = posA.x * this.pxPerM;
-            let y1 = -posA.y * this.pxPerM;
-            let x2 = posB.x * this.pxPerM;
-            let y2 = -posB.y * this.pxPerM;
+            let x1 = posA.x;
+            let y1 = posA.y;
+            let x2 = posB.x;
+            let y2 = posB.y;
 
             if (joint.getLength() > 0.01) {
                 this.ctx.beginPath();
